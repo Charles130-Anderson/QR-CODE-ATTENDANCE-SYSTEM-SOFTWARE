fb450a8e052abf4af1b22426188bc0c4be047e37
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-09-07 08:07:13 -0700

    Subsequent change to the database schema after the initial migration

---
+++ FacultyView/Migrations/2-remove_sname.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Migration for modifying 'Student' model."""
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    """Handles database migration changes."""
+
+    dependencies = [
+        ('FacultyView', '0001_initial'),
+    ]  # Dependencies for this migration
+
+    operations = [
+        migrations.RemoveField(
+            model_name='student',
+            name='s_name',
+        ),  # Remove 's_name' field from 'Student'
+
+        migrations.AddField(
+            model_name='student',
+            name='s_fname',
+            field=models.CharField(default=5, max_length=20),
+            preserve_default=False,
+        ),  # Add 's_fname' field to 'Student'
+
+        migrations.AddField(
+            model_name='student',
+            name='s_lname',
+            field=models.CharField(default=2, max_length=20),
+            preserve_default=False,
+        ),  # Add 's_lname' field to 'Student'
+    ]  # Operations to modify models
