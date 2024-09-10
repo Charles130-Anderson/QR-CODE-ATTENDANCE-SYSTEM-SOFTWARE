1199889a930d4df329cc90d948f28797f40045cb
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-09-10 12:47:00 -0700

    Making several significant changes to the database schema for the FacultyView Application

---
+++ FacultyView/migrations/3-student_s_year.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Migration for modifying 'Student' model."""
+import django.core.validators
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    """Handles database migration changes."""
+
+    dependencies = [
+        ('FacultyView', '2-remove_sname.py '),
+    ]  # Dependencies for this migration
+
+    operations = [
+        migrations.AddField(
+            model_name='student',
+            name='s_year',
+            field=models.IntegerField(
+                default=1,
+                validators=[
+                    django.core.validators.MinValueValidator(1),
+                    django.core.validators.MaxValueValidator(4)
+                ]
+            ),
+            preserve_default=False,
+        ),  # Add 's_year' field to 'Student'
+    ]  # Operations to modify models
