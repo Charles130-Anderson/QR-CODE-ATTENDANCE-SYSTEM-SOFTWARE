758ecfde2742ec338e8513d1f091fc1baee7c466
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-09-10 13:18:46 -0700

    corrections

--- StudentView/templates/models.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""
-Define models for the app.
-"""
-
-from django.db import models
-
-# Create models here
--- StudentView/templates/tests.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""
-Tests for models for the app.
-"""
-
-
-from django.test import TestCase
-
-# Create your tests here.
